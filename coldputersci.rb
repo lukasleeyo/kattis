626725ada919ac484aa5347dc1d85319dfce92ec
leejiawei <jiawei3e1@gmail.com>
2020-04-28 20:39:47 +0800

    Create coldputersci.rb

---
+++ coldputersci.rb
@@ -0,0 +1,15 @@
+# problem: https://open.kattis.com/problems/cold
+# 1.1 get input from console
+noOfTemps = gets.chomp 
+input = gets.chomp
+inputArr = input.split(' ') # split input with space and store it in array
+result = 0
+inputArr.each do |temp|
+    actualTemp = temp.to_i
+    if actualTemp < 0
+        result += 1
+    end
+end
+
+print(result)
+print("\n")
