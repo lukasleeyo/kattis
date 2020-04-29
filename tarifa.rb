af6283bd504c45a6abff3da87134b3f1d5c3e725
leejiawei <jiawei3e1@gmail.com>
2020-04-29 21:20:02 +0800

    Update tarifa.rb

--- tarifa.rb
+++ tarifa.rb
@@ -28,18 +28,14 @@
 # 3rd month: used 20 Megabytes; left 0 Megabytes; (15-20+5 = 0)
 # nth month: 15 + 0 = 15 Megabytes
 
-
 # 1.1 get input from console
 x_megabytes_month = gets.chomp.to_i
 first_n_months = gets.chomp.to_i
 megabytes_available = 0 #default megabytes left should be 0
 # prompt user for megabytes consumed per month based on first_n_months value
-first_n_months.times do 
-    p_megabytes_consumed_month = gets.chomp.to_i
-    megabytes_available += x_megabytes_month - p_megabytes_consumed_month # accumulate (AKA transfer to next month) the megabytes_available each time the mb is used from the x_megabytes_month monthly 
+first_n_months.times do
+  p_megabytes_consumed_month = gets.chomp.to_i
+  megabytes_available += x_megabytes_month - p_megabytes_consumed_month # accumulate (AKA transfer to next month) the megabytes_available each time the mb is used from the x_megabytes_month monthly
 end
 
 puts megabytes_available + x_megabytes_month # add the megabytes_available by x_megabytes_month to renew the data each month for subsequent n + 1 months
-
-
-
